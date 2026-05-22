6d6ff7f27e9e4e2a5c13c9ac3bb968026a363522
Paolo Villena <p.villena.c38@gmail.com>
2026-05-22 10:16:50 -0500

    feature: added testing library + coverage + config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+import path from "path";
+
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [vue()],
+
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			tests: path.resolve(__dirname, "./tests"),
+		},
+	},
+});
